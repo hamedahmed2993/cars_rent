7057fbf0360209856a2cf475dd3083206ccad71c
hamedahmed2993 <hamedahmed2993@gmail.com>
2026-02-07 09:43:49 +0100

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Counter from "@/components/Counter";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Counter />
+    </div>
+  );
+}
